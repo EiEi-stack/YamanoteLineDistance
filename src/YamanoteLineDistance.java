55993f8995d5a30a7d5ee1e5f028a329277b7d4c
EiEi-stack <yccvenusstudent@gmail.com>
2020-09-16 20:22:37 +0900

    最短ルートを検索更新

--- src/YamanoteLineDistance.java
+++ src/YamanoteLineDistance.java
@@ -2,30 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class YamanoteLineDistance {
-    private static String CONFIG_TIME_PATH = "C:/Users/User/Desktop/yamanote/conf/東京駅からの所要時間.txt";
-    private static String TARGET_STATION_PATH = "C:/Users/User/Desktop/yamanote/input/山手線駅間所要時間_入力.txt";
-    private static String REQUIRE_TIME_OUTPUT = "C:/Users/User/Desktop/yamanote/output/東京駅からの所要時間_出力.txt";
-    private static HashMap<String, List<Integer>> fixedTimeMap = new HashMap<String, List<Integer>>();
+    private static String DISTANCE_LIST = "C:/Users/User/Desktop/yamanote/conf/東京駅からの所要時間.txt";
+    private static String TARGET_ROUTE = "C:/Users/User/Desktop/yamanote/input/山手線駅間所要時間_入力.txt";
+    private static String DISPLAY_ROUTE = "C:/Users/User/Desktop/yamanote/output/東京駅からの所要時間_出力.txt";
+    private static HashMap<String, List<Integer>> distanceMap = new HashMap<String, List<Integer>>();
 
     public static void main(String args[]) {
-        readConfigFile(CONFIG_TIME_PATH);
-        readTargetStation(TARGET_STATION_PATH);
+        readConfigFile(DISTANCE_LIST);
+        readTargetStation(TARGET_ROUTE);
     }
 
-    private static void readConfigFile(String configTimePath) {
+    private static void readConfigFile(String distanceList) {
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(configTimePath)));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(distanceList)));
             String line = null;
             while ((line = bufferedReader.readLine()) != null) {
-                String config[] = line.split(",");
-                if (fixedTimeMap.containsKey(config[0])) {
+                String distance[] = line.split(",");
+                //東京駅からの所要時間をMapに入れる
+                if (distanceMap.containsKey(distance[0])) {
                     ArrayList<Integer> list;
-                    list = new ArrayList<>(fixedTimeMap.get(config[0]));
-                    fixedTimeMap.put(config[0], list);
+                    list = new ArrayList<>(distanceMap.get(distance[0]));
+                    list.add(Integer.parseInt(distance[1]));
+                    distanceMap.put(distance[0], list);
                 } else {
                     ArrayList<Integer> arrayList = new ArrayList<Integer>();
-                    arrayList.add(Integer.parseInt(config[1]));
-                    fixedTimeMap.put(config[0], arrayList);
+                    arrayList.add(Integer.parseInt(distance[1]));
+                    distanceMap.put(distance[0], arrayList);
                 }
             }
             bufferedReader.close();
@@ -38,21 +40,18 @@ public class YamanoteLineDistance {
 
     private static void readTargetStation(String targetStationPath) {
         try {
-            FileWriter fileWriter = new FileWriter(new File(REQUIRE_TIME_OUTPUT));
+            FileWriter fileWriter = new FileWriter(new File(DISPLAY_ROUTE));
             BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(targetStationPath)));
             String line = null;
-            StringBuilder stringBuilder = new StringBuilder();
-
             while ((line = bufferedReader.readLine()) != null) {
+                StringBuilder stringBuilder = new StringBuilder();
                 stringBuilder.append(line).append(" ");
-
                 String targetStation[] = line.split(" ", -1);
+                //最短ルートを検索する
                 int result = searchMinTime(targetStation[0], targetStation[1]);
-
                 stringBuilder.append(result).append(System.lineSeparator());
                 fileWriter.write(stringBuilder.toString());
             }
-            System.out.println(stringBuilder.toString());
             bufferedReader.close();
             fileWriter.close();
         } catch (FileNotFoundException e) {
@@ -63,26 +62,19 @@ public class YamanoteLineDistance {
     }
 
     private static int searchMinTime(String targetSt, String destinationSt) {
-        int minDistance = Integer.MAX_VALUE;
-        int minTarget = 0;
-        int minDestination = 0;
+        int minRoute = Integer.MAX_VALUE;
+        int minResult = 0;
 
-        List<Integer> targetValue = fixedTimeMap.get(targetSt);
-        List<Integer> destinationValue = fixedTimeMap.get(destinationSt);
-        for (int val : targetValue) {
-            minTarget = val;
-            if (minTarget > val) {
-                minTarget = val;
+        List<Integer> targetValue = distanceMap.get(targetSt);
+        List<Integer> destinationValue = distanceMap.get(destinationSt);
+        for (int start : targetValue) {
+            for (int end : destinationValue) {
+                minResult = Math.abs(end - start);
+                if (minRoute > minResult) {
+                    minRoute = minResult;
+                }
             }
         }
-        for (int val : destinationValue) {
-            minDestination = val;
-            if (minDestination > val) {
-                minDestination = val;
-            }
-        }
-        int result = Math.abs(minDestination - minTarget);
-
-        return result;
+        return minRoute;
     }
 }
